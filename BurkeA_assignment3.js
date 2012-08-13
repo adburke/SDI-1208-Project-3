605aabf321953a64fc03914d0aefddfd6aba1a0d
adburke <adburke@fullsail.edu>
2012-08-12 20:23:14 -0500

    JS work

--- BurkeA_assignment3.js
+++ BurkeA_assignment3.js
@@ -1,1 +1,39 @@
-alert("JavaScript works!");
+/*
+	Aaron Burke
+	08/12/2012
+	Project 3
+	Making
+*/
+
+var hockeyTeam = function (name) {
+	// Private
+	var roster = [];
+	var coach, player;
+	// Accessors
+	var getName = function () { return name; };
+	var getCoach = function () { return coach; };
+	var getPlayer = function () { return player; };
+	var getRoster = function () {
+		for (var i = 0; j = roster.length; i < j; i++) {
+			console.log(roster[i] + " is on the team.");
+		};
+	};
+	
+	// Mutators
+
+
+
+
+
+	// Public methods
+	return {
+		"name": getName,
+		"coach": getCoach,
+		"player": getPlayer,
+		"roster": getRoster,
+		"cutPlayer": cutPlayer,
+		"addPlayer": addPlayer
+
+	};
+};
+
