65f15361a09f9ac50e500dc693d4028c966e351b
adburke <adburke@fullsail.edu>
2012-08-14 23:25:16 -0500

    JS and JSON work

--- BurkeA_assignment3.js
+++ BurkeA_assignment3.js
@@ -2,7 +2,7 @@
 	Aaron Burke
 	08/12/2012
 	Project 3
-	Creating a Hockey Team with players 
+	Creating a Hockey Team with players
 */
 
 var hockeyTeam = function (name, coach) {
@@ -12,16 +12,27 @@ var hockeyTeam = function (name, coach) {
 	// Accessors
 	var getName = function () { return name; };
 	var getCoach = function () { return coach; };
-	var getPlayer = function (player) {
-		for (var i =0, j = roster.length; i < j; i++){
-			if (roster[i] === player.name(player)) {
-				var playerInfo = roster[i] + " " + "age: " + player.age() + "\n" + player.born();
-			}
+	var getPlayer = function (player,json) {
+		var playerInfo;
+		for (var i =0, j = roster.length; i < j; i++){ // loop to see if player is on roster if found break and move on
+			//console.log("got here"); // Debug check on loop
+			//console.log(roster[i]);  // Debug check on loop
+			if (roster[i] === player){
+				break;
+			};
 		};
-		if (playerInfo === undefined) {
-			return playerInfo = "Player not on the roster.";
-		} else {
+		if (roster[i] === undefined){
+			playerInfo = "Player not on roster.";
 			return playerInfo;
+		};
+		for (var ii = 0, jj = json.players.length; ii < jj; ii++){
+			//console.log("got here also");  // Debug check on loop
+			var play = json.players[ii];
+			//console.log(play.name + " " + roster[i]);  // Debug check on loop
+			if (roster[i] === play.name) {
+				playerInfo = play.name + " " + "age: " + play.age + "\n" + play.born[0] + " " + play.born[1] + "," + play.born[2];
+				return playerInfo;
+			};
 		};
 	};
 	var getRoster = function () { return roster; };
@@ -61,51 +72,15 @@ var hockeyTeam = function (name, coach) {
 	};
 };
 
-var player = function (name, age, born, number, hand, position) {
-	// Private
-	// Accessors
-	var getName = function () { return name; };
-	var getAge = function () { return age; };
-	var bornInfo = function () {
-		var bornOutput = "Born in " + born[1] + ", " + born[2] + " on " + born[0] + ".";
-		return bornOutput;
-	};
-	var playerNumber = function () {};
-	var teamPlaysFor = function () {};
-	// Public methods
-	return {
-		"name": getName,
-		"age": getAge,
-		"born": bornInfo,
-		"playerNumber": playerNumber,
-		"teamPlaysFor": teamPlaysFor
-	};
-};
-
-// Create players
-var player1 = player("Aaron Burke", 30, ["2-23-82","Lacrosse","WI"], 23, "center");
-var player2 = player("John Doe", 24, ["5-13-88","Chicago","IL"], 5, "Left Wing");
-var player3 = player("Bob Smith", 21, ["2-5-91","Baltimore","MD"], 12, "Right Wing");
-var player4 = player("Mark Jones", 30, ["1-17-82","Ancorage","AK"], 2, "Goalie");
-var player5 = player("Bill Smith", 20, ["2-23-90","St. Paul","MN"], 15, "Right Defense");
-var player6 = player("Robert White", 28, ["7-20-84","Anarbor","MI"], 23, "Left Defense");
 // Create Team
 var ravens = hockeyTeam("Ravens", "Joe Mann");
 // Add players to team
 ravens.addPlayer(["Aaron Burke", "John Doe", "Bob Smith", "Mark Jones", "Bill Smith", "Robert White"]);
 
-console.log(player1.age());
-console.log(player1.born());
-var ravens = hockeyTeam("Team 1");
-ravens.addPlayer(["Aaron Burke", "John Doe", "Bob Smith", "Mark Jones", "Bill Smith", "Robert White"]);
+
 console.log(ravens.roster());
 ravens.cutPlayer(["Bob Smith"]);
 console.log(ravens.roster());
-console.log(player1.name());
+//console.log(player1.name());
 console.log(" ");
-console.log(ravens.teamPlayer(player1));
-console.log(ravens.teamPlayer(player2));
-console.log(ravens.teamPlayer(player3)); // Cut from team should not show up on roster
-console.log(ravens.teamPlayer(player4));
-console.log(ravens.teamPlayer(player5));
-console.log(ravens.teamPlayer(player6));
+console.log(ravens.teamPlayer("Mark Jones",json));
---
+++ json.js
@@ -0,0 +1,54 @@
+// Aaron Burke 
+// SDI-1208
+// Project 3 
+// JSON data
+// Players List
+
+
+var json = {
+	"players": [
+		{
+			"name": "Aaron Burke",
+			"age": 30,
+			"hand": "Right",
+			"position": "Center",
+			"born": ["2-23-82","Lacrosse","WI"]
+		},
+		{
+			"name": "John Doe",
+			"age": 24,
+			"hand": "Left",
+			"position": "Left Wing",
+			"born": ["5-13-88","Chicago","IL"]
+		},
+		{
+			"name": "Bob Smith",
+			"age": 21,
+			"hand": "Right",
+			"position": "Right Wing",
+			"born": ["2-5-91","Baltimore","MD"]
+		},
+		{
+			"name": "Mark Jones",
+			"age": 30,
+			"hand": "Right",
+			"position": "Goalie",
+			"born": ["1-17-82","Ancorage","AK"]
+		},
+		{
+			"name": "Bill Smith",
+			"age": 20,
+			"hand": "Right",
+			"position": "Right Defense",
+			"born": ["2-23-90","St. Paul","MN"]
+		},
+		{
+			"name": "Robert White",
+			"age": 28,
+			"hand": "Left",
+			"position": "Left Defense",
+			"born": ["7-20-84","Anarbor","MI"]
+		}
+
+	]
+};
