9bb990c32f4c7390ba0ec70cf1e9b8f2cd1b4bb7
adburke <adburke@fullsail.edu>
2012-08-14 23:40:36 -0500

    JS work

--- BurkeA_assignment3.js
+++ BurkeA_assignment3.js
@@ -12,7 +12,7 @@ var hockeyTeam = function (name, coach) {
 	// Accessors
 	var getName = function () { return name; };
 	var getCoach = function () { return coach; };
-	var getPlayer = function (player,json) {
+	var getPlayer = function (player,json) {     // Takes a player name and JSON data from json.js
 		var playerInfo;
 		for (var i =0, j = roster.length; i < j; i++){ // loop to see if player is on roster if found break and move on
 			//console.log("got here"); // Debug check on loop
@@ -21,11 +21,11 @@ var hockeyTeam = function (name, coach) {
 				break;
 			};
 		};
-		if (roster[i] === undefined){
+		if (roster[i] === undefined) {
 			playerInfo = "Player not on roster.";
 			return playerInfo;
 		};
-		for (var ii = 0, jj = json.players.length; ii < jj; ii++){
+		for (var ii = 0, jj = json.players.length; ii < jj; ii++){   // Loop through JSON object of players to find info
 			//console.log("got here also");  // Debug check on loop
 			var play = json.players[ii];
 			//console.log(play.name + " " + roster[i]);  // Debug check on loop
@@ -38,7 +38,7 @@ var hockeyTeam = function (name, coach) {
 	var getRoster = function () { return roster; };
 
 	// Mutators
-	var cutPlayer = function (player) {   // player takes an array of player or players
+	var cutPlayer = function (player) {   // Function takes an array of player or players
 		for (var i = 0, j = player.length; i < j; i++) {
 			if (player[i] === "") {
 				break;
@@ -53,7 +53,7 @@ var hockeyTeam = function (name, coach) {
 			};
 		};
 	};
-	var addPlayer = function (player) {  // player takes an array of player or players
+	var addPlayer = function (player) {  // Function takes an array of player or players
 		for (var i =0, j = player.length; i<j; i++) {
 			roster.push(player[i]);
 		};
@@ -62,25 +62,26 @@ var hockeyTeam = function (name, coach) {
 
 	// Public methods
 	return {
-		"teamName": getName,
-		"teamCoach": getCoach,
-		"teamPlayer": getPlayer,
-		"roster": getRoster,
-		"cutPlayer": cutPlayer,
-		"addPlayer": addPlayer
+		"getName": getName,     // Accessor
+		"getCoach": getCoach,	// Accessor
+		"getPlayer": getPlayer,	// Accessor
+		"getRoster": getRoster,	// Accessor
+		"cutPlayer": cutPlayer, // Mutator
+		"addPlayer": addPlayer  // Mutator
 
 	};
 };
+
 
 // Create Team
 var ravens = hockeyTeam("Ravens", "Joe Mann");
 // Add players to team
 ravens.addPlayer(["Aaron Burke", "John Doe", "Bob Smith", "Mark Jones", "Bill Smith", "Robert White"]);
+console.log(ravens.getName());
 
-
-console.log(ravens.roster());
+console.log(ravens.getRoster());
 ravens.cutPlayer(["Bob Smith"]);
-console.log(ravens.roster());
+console.log(ravens.getRoster());
 //console.log(player1.name());
 console.log(" ");
-console.log(ravens.teamPlayer("Mark Jones",json));
+console.log(ravens.getPlayer("Robert White",json));
