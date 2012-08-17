32f8f94841bfaea73cd923ad0d16e92f6125af85
adburke <adburke@fullsail.edu>
2012-08-16 22:04:38 -0500

    JS work and JSON change

--- BurkeA_assignment3.js
+++ BurkeA_assignment3.js
@@ -9,11 +9,11 @@ var teams = [];  // Holds an array of hockeyTeam objects created
 
 // Constructor 
 var HockeyTeam = function (name, coach) {
-	// Private
+	var injuries, injuredNum = 0;
 	var roster = [];
 	// Accessors
-	var getName = function () { return name; };
-	var getCoach = function () { return coach; };
+	var getName = function () { return name; };   // String return
+	var getCoach = function () { return coach; }; // String return
 	var getPlayer = function (player, json) {     // Takes a player name and JSON data from json.js
 		var playerInfo;
 		for (var i = 0, j = roster.length; i < j; i++) { // loop to see if player is on roster if found break and move on
@@ -25,7 +25,7 @@ var HockeyTeam = function (name, coach) {
 		};
 		if (roster[i] === undefined) {
 			playerInfo = "Player not on roster.";
-			return playerInfo;
+			return playerInfo; // String return
 		};
 		for (var ii = 0, jj = json.players.length; ii < jj; ii++) {   // Loop through json object of players to find info
 			//console.log("got here also");  // Debug check on loop
@@ -34,14 +34,17 @@ var HockeyTeam = function (name, coach) {
 			if (roster[i] === play.name) {
 				playerInfo = play.name + " - " + "Age: " + play.age + " - " + "Born: " + play.born[0] + " in " + play.born[1] + "," + play.born[2]
 					+ "\n" + "Position: " + play.position + " - " + "Shoots: " + play.hand;
-				return playerInfo;
+				return playerInfo; // String return
 			};
 		};
+		playerInfo = player + " not found on player data list.";
+		return playerInfo;
 	};
-	var getRoster = function () { return roster; };
+	var getRoster = function () { return roster; }; // Array return
 
 	// Mutators
 	var cutPlayer = function (player) {   // Function takes an array of player or players
+		var cut = 0;
 		for (var i = 0, j = player.length; i < j; i++) {
 			if (player[i] === "") {
 				break;
@@ -53,17 +56,20 @@ var HockeyTeam = function (name, coach) {
 					//console.log(playerRemove);  // Debug usage to check index location
 					roster.splice(playerRemove,1);
 					console.log(player + " removed from " + name + " roster.");
+					cut++;
 				};
 			};
 		};
-		return roster;
+		return cut; // Number return
 	};
 	var addPlayer = function (player) {  // Function takes an array of player or players
+		var added = 0;
 		for (var i =0, j = player.length; i<j; i++) {
 			roster.push(player[i]);
 			console.log(player[i] + " added to " + name + " roster.");
+			added++;
 		};
-		return roster;
+		return added; // Number return
 	};
 	var playGame = function (homeTeam,awayTeam){    // Game simulation every play has a different outcome
 		var outCome = [];
@@ -84,9 +90,9 @@ var HockeyTeam = function (name, coach) {
 		};
 		outCome.push([homeTeam,hScore]);
 		outCome.push([awayTeam,aScore]);
-		return outCome;
+		return outCome;  // 2D Array return
 	};
-	var playedCollege  = function () {
+	var playedCollege  = function () {  // Checks to see if team has players that played college
 		var collegePlayers;
 		for (var i = 0, j = roster.length; i < j; i++){
 			for (var ii = 0, jj = playerData.players.length; ii < jj; ii++){
@@ -99,11 +105,22 @@ var HockeyTeam = function (name, coach) {
 				};
 			};
 		};
-		return collegePlayers;
+		return collegePlayers; // Boolean return
 	};
-
+	var setInjuries = function (boolinput, number) {
+		if (boolinput === true && isNaN(number) === false) {
+			injuredNum = number;
+			injuries = boolinput;
+		} else if (boolinput === false || isNaN(number) === true) {
+			injuries === false;
+			console.log(number + " is not a number.");
+		} else {
+			console.log("Not a true or false input");
+		};
+	};
+	var getInjuries = function () { return injuredNum; };
 	// Public methods
-	return {
+	return {   // Object return
 		"getName": getName,     // Accessor
 		"getCoach": getCoach,	// Accessor
 		"getPlayer": getPlayer,	// Accessor
@@ -111,7 +128,9 @@ var HockeyTeam = function (name, coach) {
 		"cutPlayer": cutPlayer, // Mutator
 		"addPlayer": addPlayer, // Mutator
 		"playGame": playGame,
-		"haveCollegePlayers": playedCollege
+		"haveCollegePlayers": playedCollege,
+		"setInjuries": setInjuries,
+		"getInjuries": getInjuries
 
 	};
 };
@@ -153,10 +172,16 @@ if (team2.haveCollegePlayers() === true){
 	console.log("No they do not.");
 };
 console.log(" ");
+// Set some injuries
+team1.setInjuries(true, 1);
+team2.setInjuries(true, 1);
+console.log(team1.getName() + " injury count: " + team1.getInjuries());
+console.log(team2.getName() + " injury count: " + team2.getInjuries());
+console.log(" ");
 // Cut some players
 console.log("Looks like we need to cut some players before the big game.");
-team1.cutPlayer(["Bob Smith"]);
-team2.cutPlayer(["Matt Black"]);
+console.log("How many were cut from the " + team1.getName() + " roster? " + team1.cutPlayer(["Bob Smith"]));
+console.log("How many were cut from the " + team2.getName() + " roster? " + team2.cutPlayer(["Matt Black"]));
 console.log(" ");
 // Pickup some new players 
 console.log("Time to pick up some better help.")
@@ -172,7 +197,7 @@ console.log(" ");
 var outCome = team1.playGame(team1.getName(),team2.getName());
 console.log("Game score: " + outCome[0][0] + " - " + outCome[0][1] + " : " + outCome[1][0] + " - " + outCome[1][1]);
 console.log(" ");
-team1.haveCollegePlayers();
+
 
 
 
